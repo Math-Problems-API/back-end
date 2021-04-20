eeb54c8a9844b55f1788327b5630265fcb3632b4
sformichella <sformichella@gmail.com>
2021-04-20 02:24:10 -0700

    gql setup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./lib/app');
 
-app.listen(7890, () => {
-  console.log('Application started!');
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  console.log(`Server started! Running on ${PORT}.`);
 });
