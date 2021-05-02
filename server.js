6e48c241f6fe4037a0828295bebff8d5c23df00d
sformichella <sformichella@gmail.com>
2021-05-02 12:18:59 -0700

    add database stuff

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const app = require('./lib/app');
+const pool = require('./lib/utils/pool');
 
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   console.log(`Server started! Running on ${PORT}.`);
+});
+
+process.on('exit', () => {
+  console.log('Goodbye!');
+  pool.end();
 });
