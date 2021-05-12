b3288d4a60f9034261292fee872c170e199b7de5
sformichella <sformichella@gmail.com>
2021-05-12 14:31:16 -0700

    binary operator problem schema stuff

--- lib/graphql/schema/index.ts
+++ lib/graphql/schema/index.ts
@@ -1,8 +1,27 @@
 import { buildSchema } from "graphql";
 
 export default buildSchema(`
+  type Problem {
+    problem: String!
+    solution: String!
+  }
+
+  type Link {
+    modifier: Int
+    target: Int!
+    constraints: [String!]!
+  }
+
+  input BinaryOperatorInput {
+    operands: [String!]!
+    operator: String!
+    links: [Link!]
+    number: Int
+  }
+
   type RootQuery {
     helloWorld: String!
+    binaryOperator(binaryOperatorInput: BinaryOperatorInput): [Problem!]!
   }
 
   schema {
