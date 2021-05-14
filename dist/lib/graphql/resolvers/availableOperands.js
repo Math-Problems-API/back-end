c2a407e39c24238eb8a873fbbd423626c23be7e3
sformichella <sformichella@gmail.com>
2021-05-14 15:01:03 -0700

    latest build

--- dist/lib/graphql/resolvers/availableOperands.js
+++ dist/lib/graphql/resolvers/availableOperands.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("../../api/operands/index"));
+const index_1 = __importDefault(require("../../problemsAPI/operands/index"));
 exports.default = {
     availableOperands: () => index_1.default
 };
