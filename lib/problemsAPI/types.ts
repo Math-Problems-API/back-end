e15d2996843e7c4b64fa379416d2625072c78c7c
sformichella <sformichella@gmail.com>
2021-05-18 16:39:56 -0700

    orgnize types a bit

--- lib/problemsAPI/types.ts
+++ lib/problemsAPI/types.ts
@@ -1,28 +1,21 @@
-// Specific instance of a RandomOperand
+// Generating Problems
 export type Operand = {
   value: string | number
 }
 
-// Problem shape. solution is optional; not assuming
-// we'll be able to generate solutions for every
-// problem
 export type Problem = {
   problem: string | number,
   solution?: string | number
 }
 
-// Properties are used according to the definition of
-// a generator function
-export type Property = {
-  value: number | number[] | string | string[]
-}
+export type Operator = (props: Operand[]) => Problem;
+
+export type Generator = (ops: RandomOperand) => Operand;
+export type GroupGenerator = (ops: RandomOperand[]) => Operand[];
+export type GroupsGenerator = (ops: RandomOperand[][]) => Operand[][];
 
 
-export type Constraint = (value: Operand) => boolean;
-
-
-// Generates an Operand according to its given
-// properties and the definition of the generator
+// Generating Operands
 export type RandomOperand = {
   name?: string,
   generator: (properties: Property[]) => Operand,
@@ -31,6 +24,22 @@ export type RandomOperand = {
   value?: string | number
 }
 
+export type Property = {
+  value: number | number[] | string | string[]
+}
+
+export type Constraint = (value: Operand) => boolean;
+
+export type Link = {
+  modifier: number,
+  target: number,
+  constraints: FirstOrderConstraint[]
+}
+
+export type FirstOrderConstraint = (modifier: Operand) => Constraint;
+
+
+// Parsing GraphQL Data
 export type UnParsedRandomOperand = {
   name: string,
   constraints?: string[],
@@ -42,20 +51,3 @@ export type UnParsedLink = {
   modifier: number,
   constraints: string[]
 }
-
-
-// Generic Operator shape
-// e.g. addition is ([left, right]) => `${left} + ${right}`
-export type Operator = (props: Operand[]) => Problem;
-
-export type Generator = (ops: RandomOperand) => Operand;
-export type GroupGenerator = (ops: RandomOperand[]) => Operand[];
-export type GroupsGenerator = (ops: RandomOperand[][]) => Operand[][];
-
-export type Link = {
-  modifier: number,
-  target: number,
-  constraints: FirstOrderConstraint[]
-}
-
-export type FirstOrderConstraint = (modifier: Operand) => Constraint;
